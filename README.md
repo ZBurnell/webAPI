1f5c79583e363373643da4f797e4924f6a260c7d
ZBurnell <136769106+ZBurnell@users.noreply.github.com>
2023-09-18 23:32:57 -0400

    Added to the README.md

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 ## webAPI
 
-## Acknowledgment
-css functions were gathered from bootstrap.com
+# Description
+WebAPI project. This is supposed to be a multiple choice quiz. I did not get it to a working state in the alloted amount of time. I have the questions formatted as buttons right now in the HTML. I would like to continue to work and develope the quiz using the proper click functions in JS rather than having them listed in the HTML. as well as getting the timer and alert working.
 
-## License 
-MIT License. Can be found in the Repository
+# Installation
+To view the webpage from this Repo. Settings > Pages > Visit Site or Use this website -> https://zburnell.github.io/webAPI
+
+# Support
+for any questions feel free to email me at zackburnell922@gmail.com
+
+# Acknowledgment
+https://jquery.com/ and https://getbootstrap.com/ were both used as a resource
+
+# License
+MIT License. License can also be found in the Repo.  
--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,36 @@
-var score = 0;
-clock = 60;
-clockSpan.textContent = clock;
-
+document.getElementById("start-quiz").addEventListener("click", startQuiz);
 
 function quiz(event){
     event.preventdeault;
     questionbank = []
 
 
-    var Q1 = {question : "what does :WQ do to the VIM?"
-    choices: ["1: write and quit", "2: won't quit", "3: wrap and quit", "4: weird and quiet"]
+    var Q1 = {question : "what does :WQ do to the VIM?",
+    choices: ["1: write and quit", "2: won't quit", "3: wrap and quit", "4: weird and quiet"],
     answer: "option 1"}; 
     questionbank.push(Q1)
 
-    var Q2 = {question: "Arrays in JavaScript can be used to store what?"
-    choices: ["1: numbers and strings", "2: other arrays", "3: booleans", "4: all of the above"]
+    var Q2 = {question: "Arrays in JavaScript can be used to store what?",
+    choices: ["1: numbers and strings", "2: other arrays", "3: booleans", "4: all of the above"],
     answer: "option 4"};
     questionbank.push(Q2)
 
-    var Q3 = {question: "Commonly used data types DO NOT include"
-    choices: ["1: strings", "2: alerts", "3: booleans", "4: numbers"]
+    var Q3 = {question: "Commonly used data types DO NOT include",
+    choices: ["1: strings", "2: alerts", "3: booleans", "4: numbers"],
     answer: "option 3"};
     questionbank.push(Q3)
 
-    var Q4 = {question: "How to write an IF statement in Javascript?"
-    choices: ["1: if i = 5 then"; "2: if i = 5"; "3: if i == 5 then"; "4: if (i==5)";]
+    var Q4 = {question: "How to write an IF statement in Javascript?",
+    choices: ["1: if i = 5 then", "2: if i = 5", "3: if i == 5 then", "4: if (i==5)"],
     answer: "option 3"};
     questionbank.push(Q4)
 }
-    
+
+var lastQuestion = questionbank.length - 1;
+var score = 0;
+countDown = 75;
+
+
 for (var i=o; i < question.length; i++){
     var response = window.();
     if(response==[i].answer){
@@ -39,22 +41,22 @@ for (var i=o; i < question.length; i++){
     }
 }
 
-
-function time() {
+function setTime() {
     var timerInterval = setInterval(function () {
-        clock--;
-        clockSpan.textContent = clock;
+        countDown--;
+        countDownSpan.textContent = countDown;
         if (countDown === 0) {
             clearInterval(timerInterval);
-            sorry try again();
-        } else if (clock < 0) {
+            gameOver();
+        } else if (countDown < 0) {
             clearInterval(timerInterval);
-            sorry try again();
-            clock = 0
+            gameOver();
+            countDown = 0
+        }
+        else if (currentQuestionIndex === lastQuestionIndex) {
+            clearInterval(timerInterval);
         }
     }, 1000);
 };
 
-
-
-
+setTime()
